66b75b8a237f7423b4a8b090c6b3cb1f94ac1c63
shahid662021 <shahid_jamil_aligarh@yahoo.com>
2021-01-18 12:47:39 +0500

    Zuhaib Ahmed

---
+++ Introduction.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php    
+    $num = 5;
+    
+    for($i = 1;$i <= 10 ;$i++)
+    {
+        $result = $num *$i;        
+        echo $num . " X " .$i . " = " .$result . "<br>";
+    }            
+    ?>
+       
+</body>
+</html>
